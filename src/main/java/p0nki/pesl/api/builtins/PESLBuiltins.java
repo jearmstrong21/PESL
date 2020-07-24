36498a1be299bd66285c19d4586741f71dabb189
valoeghese <46117728+valoeghese@users.noreply.github.com>
2020-07-25 05:14:15 +1200

    Unfuck Noise (#2)

--- src/main/java/p0nki/pesl/api/builtins/PESLBuiltins.java
+++ src/main/java/p0nki/pesl/api/builtins/PESLBuiltins.java
@@ -115,8 +115,8 @@ public class PESLBuiltins {
             .put("noise", FunctionObject.of(false, arguments -> {
                 PESLEvalException.validArgumentListLength(arguments, 1, 2);
                 double y = 0;
-                if (arguments.size() == 2) y = arguments.get(0).asNumber().getValue();
-                return new NumberObject(OpenSimplexNoise.PESL_INSTANCE.sample(arguments.get(0).asNumber().getValue()));
+                if (arguments.size() == 2) y = arguments.get(1).asNumber().getValue();
+                return new NumberObject(OpenSimplexNoise.PESL_INSTANCE.sample(arguments.get(0).asNumber().getValue(), y));
             }));
 
     public static final PESLObject DATA = BuiltinMapLikeObject.builtinBuilder()
